c8faa03e450a21e8433fde585d729e26b5655a25
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2025-06-20 13:50:00 +0100

    Chore: added new server function

--- src/lib/actions.js
+++ src/lib/actions.js
@@ -8,3 +8,32 @@ export async function deletePost(id) {
   revalidatePath("/posts");
   redirect("/posts");
 }
+
+export async function createComment(id, formData) {
+  const commentData = {
+    username: formData.get("username"),
+    comment: formData.get("comment"),
+  };
+
+  await db.query(
+    `INSERT INTO comments (post_id, username, comment) Values($1, $2, $3)`,
+    [id, commentData.username, commentData.comment]
+  );
+  revalidatePath(`/posts/${id}`);
+  redirect(`/posts/${id}`);
+}
+
+export async function updatePost(id, formData) {
+  const postData = {
+    title: formData.get("title"),
+    content: formData.get("content"),
+  };
+
+  await db.query(`UPDATE posts SET title = $1, content = $2 WHERE id = $3`, [
+    postData.title,
+    postData.content,
+    id,
+  ]);
+  revalidatePath(`/posts/${id}`);
+  redirect(`/posts/${id}`);
+}
